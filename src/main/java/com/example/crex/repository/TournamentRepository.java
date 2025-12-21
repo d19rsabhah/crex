48ba69024f1bf76413f11cf824caaf3ce575b5f9
unknown <rishavdas27602@gmail.com>
2025-12-21 11:00:05 +0530

    tournament implemented

--- src/main/java/com/example/crex/repository/TournamentRepository.java
+++ src/main/java/com/example/crex/repository/TournamentRepository.java
@@ -3,5 +3,11 @@ package com.example.crex.repository;
 import com.example.crex.model.entity.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
+    Optional<Tournament> findByTitleIgnoreCase(String title);
+
+    List<Tournament> findByTitleContainingIgnoreCase(String title);
 }
